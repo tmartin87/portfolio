370e67e48deff9acd431281c3209aba59afcf541
Tomás Martín <tmartindsgn@gmail.com>
2026-04-06 15:53:33 +0200

    projects using template

---
+++ src/scripts/templates.js
@@ -0,0 +1,28 @@
+import { projects } from "./data.js";
+
+const templateProject = document.querySelector(
+    '[data-template="project-card"]');
+
+const projectsUL = document.querySelector("#projects ul");
+
+projects.forEach((project) => {
+    const card = templateProject.content.cloneNode(true);
+    const title = card.querySelector("h4");
+    const image = card.querySelector("img");
+    const description = card.querySelector("p");
+    const tech = card.querySelector(".tech");
+    const link = card.querySelector("a");
+
+    Image.src = project.image.src;
+    Image.alt = project.image.alt;
+
+    title.textContent = project.title;
+    description.textContent = project.description;
+
+    tech.textContent = project.tech;
+    link.href = project.link;
+
+    projectsUL.appendChild(card);
+    
+
+})

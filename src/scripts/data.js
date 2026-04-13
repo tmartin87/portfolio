4e657380a246984fe2596d1489e87153dffab03f
Tomás Martín <tmartindsgn@gmail.com>
2026-04-13 19:50:28 +0200

    new comments added

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -40,19 +40,19 @@ export const projects = [
 export const reviews = [
   {
     image: {
-      src: "https://i.pravatar.cc/150?img=3",
-      alt: "Foto de perfil de Juan Pérez",
+      src: "https://media.licdn.com/dms/image/v2/D4D03AQE1_0-wtvuJUQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1715771220764?e=1777507200&v=beta&t=0Aq2LjxicDo2blRrOt1Q8JG711m2RILkjdCNRVW3xIc",
+      alt: "Foto de perfil de Meritxell Gabarra",
     },
-    name: "Juan Pérez",
-    comment: "Excelente trabajo, superó mis expectativas. La comunicación fue fluida y el proyecto se entregó a tiempo. ¡Recomiendo sus servicios sin dudarlo!",
+    name: "Meritxell Gabarra",
+    comment: "Recomiendo a Tomás no solo por su capacidad de trabajo, sino por su excelente actitud. Tras 8 años trabajando juntos, a pesar de desempeñar funciones distintas, he sido testigo de su capacidad para enfrentar retos bajo presión sin perder nunca la sonrisa ni las ganas de ayudar a los demás. Es un trabajador que se adapta fácilmente a cualquier nuevo reto y siempre está dispuesto a aprender algo nuevo.¿un gran profesional y mejor compañero!",
   },
   {
     image: {
-      src: "https://i.pravatar.cc/150?img=3",
-      alt: "Foto de perfil de Juan Pérez",
+      src: "https://media.licdn.com/dms/image/v2/D4D03AQEegCaWlnmlHA/profile-displayphoto-shrink_400_400/B4DZdlXhFdHkAg-/0/1749752353544?e=1777507200&v=beta&t=z8vs_oDNSS_aGmqQAZasZmxYDkiFrVVikFxKabzcX3s",
+      alt: "Foto de perfil de Marta Gómez Martínez",
     },
-    name: "Juan Pérez",
-    comment: "Excelente trabajo, superó mis expectativas. La comunicación fue fluida y el proyecto se entregó a tiempo. ¡Recomiendo sus servicios sin dudarlo!",
+    name: "Marta Gómez Martínez",
+    comment: "Tuve la suerte de comartir equipo con Tomás durante el bootcamp de desarrollo web en Ironhack. Desde el primer día demostró ser una persona increíblemente trabajadora y comprometida. Tiene una gran capacidad de enfrentarse a los retos, no se rende hasta entender cada detalle y siempre busca la excelencia en todo lo que hace. Además, es un gran compañero. siempre dispuesto a ayudar y a sumar al equipo. Sin duda, alguien con quien querrias trabajar.",
   },
   {
     image: {

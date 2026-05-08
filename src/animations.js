5ce2600425bfb8a031f77f1d193b5d92e40a3e05
Tomás Martín <tmartindsgn@gmail.com>
2026-05-08 12:51:06 +0200

    animations2

--- src/animations.js
+++ src/animations.js
@@ -1,7 +1,7 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('visible')
+      entry.target.classList.add('is-visible')
     }
   })
 }, { threshold: 0.1 })
